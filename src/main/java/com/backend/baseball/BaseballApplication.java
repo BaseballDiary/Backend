46eef89254bf7ec434ecfe138799fce8c6b36be4
cinsy26 <siusy2618@naver.com>
2025-01-31 17:56:04 +0900

    Feat: mysql

--- src/main/java/com/backend/baseball/BaseballApplication.java
+++ src/main/java/com/backend/baseball/BaseballApplication.java
@@ -2,10 +2,17 @@ package com.backend.baseball;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class BaseballApplication {
-
+	static {
+		// .env 파일 로드
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(BaseballApplication.class, args);
 	}
