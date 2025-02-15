5604865b3d99e38415235dc38cfba3d62ac1636f
heehoonhong <gmlgns0522@naver.com>
2025-02-15 13:05:43 +0900

    프론트 배포 페이지 허용 수정

--- src/main/java/com/backend/baseball/Config/WebConfig.java
+++ src/main/java/com/backend/baseball/Config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://bbd-seven.vercel.app/login/sign-up")  // 모든 Origin 허용
+                        .allowedOrigins("https://bbd-seven.vercel.app")  // 모든 Origin 허용
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
