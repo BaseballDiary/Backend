99296bba744d1372ec9ce015e2bdbef525427003
cinsy26 <siusy2618@naver.com>
2025-02-16 05:14:17 +0900

    FIx: log 추가

--- src/main/java/com/backend/baseball/User/config/WebSecurityConfig.java
+++ src/main/java/com/backend/baseball/User/config/WebSecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
