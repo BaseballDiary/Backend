998ee56f3d6a4cadf14eda701888f2240ba4183f
cinsy26 <siusy2618@naver.com>
2025-02-18 13:43:51 +0900

    Feat: home controller home/info 구현, diary 수정

---
+++ src/main/java/com/backend/baseball/Home/entity/Attendance.java
@@ -0,0 +1,33 @@
+package com.backend.baseball.Home.entity;
+
+import com.backend.baseball.User.entity.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
+@Entity
+public class Attendance {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long attCertificateId; //출석 식별 번호
+
+    @ManyToOne
+    @JoinColumn(name = "certificateId", nullable = false)
+    private User user; //회원 식별 번호
+
+    @Column(nullable = false)
+    private LocalDate date; //날짜 식별 번호
+
+    @Column(nullable = false)
+    private Boolean isAttendance; //출석 여부
+
+}
