0e161f1cbb1b3aef3c70e0175f1c678923f1f517
heehoonhong <gmlgns0522@naver.com>
2025-02-20 16:28:52 +0900

    add: 닉네임 중복 api 추가

---
+++ src/main/java/com/backend/baseball/User/dto/NicknameDupDto.java
@@ -0,0 +1,15 @@
+package com.backend.baseball.User.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
+public class NicknameDupDto {
+
+    private String nickname;
+}
